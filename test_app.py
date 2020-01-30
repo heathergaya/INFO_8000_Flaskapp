f97a691d9be8b76ba02115b725440ced93e16156
Heather Gaya <Heather.e.gaya@gmail.com>
2020-01-30 15:11:52 -0500

    first commit

---
+++ test_app.py
@@ -0,0 +1,8 @@
+from flask import Flask, escape, request, jsonify
+
+app = Flask(__name__)
+@app.route('/', methods = ['GET', 'POST'])
+def hello():
+    name = request.args.get("name", "World")
+    response = {"name":name}
+    return jsonify(response)
