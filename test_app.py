f9749e364fab52b7fca1cc27a321720672a6ea54
Heather Gaya <46654652+heathergaya@users.noreply.github.com>
2020-02-07 15:34:27 -0500

    Delete test_app.py

--- test_app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, escape, request, jsonify
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    response = {"name":name}
-    return jsonify(response)
