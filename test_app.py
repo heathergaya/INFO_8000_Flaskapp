3a5fbe7fb42281fb96ceb03b70521b4bef4e23fd
Heather Gaya <46654652+heathergaya@users.noreply.github.com>
2020-02-07 15:05:46 -0500

    Add files via upload
    This is very frustrating

--- test_app.py
+++ test_app.py
@@ -5,12 +5,13 @@ from flask import g
 DATABASE = 'birdinfo.db'
 
 app = Flask(__name__)
-@app.route('/grabdata/', methods = ['GET'])
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return jsonify(db)
+
+#@app.route('/grabdata/', methods = ['GET'])
+#def get_db():
+#    db = getattr(g, '_database', None)
+#    if db is None:
+#        db = g._database = sqlite3.connect(DATABASE)
+#    return jsonify(db)
 
 @app.route('/')
 def hello():
