fc5a6210f89043c08d58ce23ec3af08c0c1ffd6c
Heather Gaya <46654652+heathergaya@users.noreply.github.com>
2020-02-07 15:34:41 -0500

    Add files via upload

---
+++ test_app.py
@@ -0,0 +1,9 @@
+from flask import Flask, escape, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    response = {"name":name + "Hello"}
+    return jsonify(response)
