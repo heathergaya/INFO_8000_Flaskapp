b8084e8d3b9155be1dcca62cf86c692359d886ff
Heather Gaya <46654652+heathergaya@users.noreply.github.com>
2020-02-07 14:36:05 -0500

    Add files via upload
    tried to connect to database

--- test_app.py
+++ test_app.py
@@ -1,8 +1,13 @@
 from flask import Flask, escape, request, jsonify
+import sqlite3
+from flask import g
+
+DATABASE = '/home/heather_e_gaya/INFO_8000_Flaskapp/birdinfo.db'
 
 app = Flask(__name__)
-@app.route('/', methods = ['GET', 'POST'])
-def hello():
-    name = request.args.get("name", "World")
-    response = {"name":name}
-    return jsonify(response)
+@app.route('/grabdata/', methods = ['GET'])
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return jsonify(db)
