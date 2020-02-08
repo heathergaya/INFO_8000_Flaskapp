7b56e530f956572e59bc70140b58294d22b3cfc9
Heather Gaya <46654652+heathergaya@users.noreply.github.com>
2020-02-08 16:36:36 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -14,7 +14,7 @@ def hello():
 
 
 
-@app.route('/testconnect/')
-def get_db():
-  db = g._database = sqlite3.connect(DATABASE)
-    return jsonify({"I connected"})
+#@app.route('/testconnect/')
+#def get_db():
+#  db = g._database = sqlite3.connect(DATABASE)
+#    return jsonify({"I connected"})
