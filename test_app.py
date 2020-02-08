04512ed89e935557386005f80693991631767689
Heather Gaya <46654652+heathergaya@users.noreply.github.com>
2020-02-08 16:10:26 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,4 +1,8 @@
 from flask import Flask, escape, request, jsonify
+import sqlite3
+from flask import g
+
+DATABASE = 'birdinfo.db'
 
 app = Flask(__name__)
 
@@ -7,3 +11,12 @@ def hello():
     name = request.args.get("name", "World")
     response = {"name":name + "Hello"}
     return jsonify(response)
+
+
+
+@app.route('/getdata/')
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return jsonify(db)
