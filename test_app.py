fc610df26c72786da47dd82ed131bff09e2269fa
Heather Gaya <46654652+heathergaya@users.noreply.github.com>
2020-02-08 16:55:15 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -15,12 +15,7 @@ def hello():
 
 
 @app.route('/testconnect/')
-def get_db():
-    db = getattr(g, '_database', None)
-     name = request.args.get("name", "World")
-    response = {"name":name + " Hello"}
-    if db is None:
-        database_file = current_app.config['DATABASE']
-        db = g._database = sqlite3.connect(database_file)
-        db.row_factory = sqlite3.Row
+def hello2():
+    name = request.args.get("name", "World")
+    response = {"name":name + " I am angry that this isn't working"}
     return jsonify(response)
